4ccc121765d0e1d7c9509a4a8004233312d063a7
Tim <timothy.c0001@gmail.com>
2025-11-17 07:48:49 -0800

    WalkerPath - Added close WorldMap before Walking

--- plugins/src/main/java/com/tonic/plugins/HH1/ExamplePlugin.java
+++ plugins/src/main/java/com/tonic/plugins/HH1/ExamplePlugin.java
@@ -1,19 +1,9 @@
 package com.tonic.plugins.HH1;
 
-import com.tonic.Logger;
-import com.tonic.api.entities.PlayerAPI;
-import com.tonic.data.wrappers.ActorEx;
-import com.tonic.data.wrappers.PlayerEx;
-import com.tonic.queries.PlayerQuery;
-import com.tonic.services.GameManager;
 import com.tonic.services.pathfinder.Walker;
 import com.tonic.util.VitaPlugin;
-import net.runelite.api.Player;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.plugins.PluginDescriptor;
-
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @PluginDescriptor(
         name = "HH1 Example",
@@ -29,24 +19,6 @@ public class ExamplePlugin extends VitaPlugin {
         while (t == 0) {
             Walker.walkTo(new WorldPoint(3096, 3236, 0));
             t++;
-            Logger.info(" " + PlayerEx.getLocal().getPlayer());
-            Logger.info(""+ PlayerAPI.search());
-            Logger.info(""+ GameManager.playerList().get(0).getPlayer().getName());
-            Logger.info(""+ GameManager.playerList()
-                    .stream()
-                    .map(PlayerEx::getName)
-                    .collect(Collectors.toList()));
-            Logger.info(""+ GameManager.playerList()
-                    .stream()
-                    .filter(playerEx -> !playerEx.getPlayer().getName().equals(PlayerEx.getLocal().getPlayer().getName()))
-                    .map(player -> player.getPlayer().getName() )
-                    .collect(Collectors.toList()));
-            Logger.info(""+ GameManager.playerList()
-                    .stream()
-                    .filter(p -> !Objects.equals(p.getPlayer().getName(), PlayerEx.getLocal().getPlayer().getName()))
-                    .map(player -> player.getPlayer().getName() )
-                    .collect(Collectors.toList()));
-            Logger.info(""+ PlayerAPI.search().withName("Honsinoa"));
         }
     }
 
